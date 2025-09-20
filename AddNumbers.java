a6b12011b639a1331154c1d4ac56fee7186b82e5
VarshaMK08 <varsha2006mk@gmail.com>
2025-09-20 10:16:39 +0530

    Add AddNumbers.java

--- AddNumbers.java
+++ AddNumbers.java
@@ -1,7 +1,7 @@
 public class AddNumbers {
     public static void main(String[] args) {
         int a = 5;
-        int b = 5;
+        int b = 6;
         int sum = a+b;
         System.out.println(sum);
     }
